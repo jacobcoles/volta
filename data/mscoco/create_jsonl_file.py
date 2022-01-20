e8f704c93e06a196b048f23165e4e808fce10275
Jacob Coles <jacob.coles@gmail.com>
2022-01-20 18:03:32 +0100

    fixed create_jsonl_file.py

--- data/mscoco/create_jsonl_file.py
+++ data/mscoco/create_jsonl_file.py
@@ -1,8 +1,18 @@
 import os
+from pycocotools.coco import COCO
+import json
+dataDir = '../../../aics-project/data/coco_annotations'
+dataType = 'val2017'
+annInstancesFile = '{}/instances_{}.json'.format(dataDir,dataType)
+coco_instances = COCO(annInstancesFile)
+annCaptionsFile = '{}/captions_{}.json'.format(dataDir,dataType)
+coco_caps=COCO(annCaptionsFile)
+
+
 faster_r_cnn_output = "./images/coco_images/test2015"
 img_ids = list()
 
-
+ann_count = 0
 with open("output.jsonl", 'w') as f:
     for file in os.listdir(faster_r_cnn_output):
         img_id = int(file.strip('.npy'))
