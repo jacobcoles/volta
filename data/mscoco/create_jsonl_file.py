2f023840827f2377d1731a9551f1a5548c12bf8c
Nikolai Ilinykh <xilini@GU.GU.SE@mltgpu.flov.gu.se>
2022-01-21 10:12:49 +0100

    minor edits

--- data/mscoco/create_jsonl_file.py
+++ data/mscoco/create_jsonl_file.py
@@ -1,7 +1,9 @@
 import os
-from pycocotools.coco import COCO
+from pycocotools_src.coco import COCO
 import json
-dataDir = '../../../aics-project/data/coco_annotations'
+import glob
+
+dataDir = '/srv/data/aics/datasets/mscoco/annotations/'
 dataType = 'val2017'
 annInstancesFile = '{}/instances_{}.json'.format(dataDir,dataType)
 coco_instances = COCO(annInstancesFile)
@@ -9,13 +11,13 @@ annCaptionsFile = '{}/captions_{}.json'.format(dataDir,dataType)
 coco_caps=COCO(annCaptionsFile)
 
 
-faster_r_cnn_output = "./images/coco_images/test2015"
+faster_r_cnn_output = "/home/guscoleja@GU.GU.SE/volta/data/mscoco/images/coco_images/test2015/"
 img_ids = list()
 
 ann_count = 0
 with open("output.jsonl", 'w') as f:
-    for file in os.listdir(faster_r_cnn_output):
-        img_id = int(file.strip('.npy'))
+    for file in glob.glob(faster_r_cnn_output + '*.npy'):
+        img_id = int(file.split('/')[-1].split('.npy')[0])
         ann_ids = coco_caps.getAnnIds(imgIds=img_id)
         annotations = coco_caps.loadAnns(ann_ids)
         sentences = list()
